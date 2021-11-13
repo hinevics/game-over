ea69c9e4e82275635846800246f70c94333700ac
Sasha Khinevich <hinevics@gmail.com>
2021-11-14 00:36:14 +0300

    update temp, config, app

--- game_over/app/app.py
+++ game_over/app/app.py
@@ -3,7 +3,7 @@ import datetime
 # import re
 # import random
 
-from config import PATH_PLAYERS, PATH_TEMP_NEW_USER, RATING
+from config import PATH_PLAYERS, PATH_TEMP_NEW_USER, RATING, PATH_TEMP_START_STATUS
 # LIVES, RATING, PATH_DB, PATH_PRINT_GAME_OVER,
 
 
@@ -12,17 +12,24 @@ def parser_data_user(user):
     pass
 
 
+def new_user(name: str) -> tuple:
+    PATH_NEW_USER = r"{path}\{user_name}".format(path=PATH_PLAYERS, user_name=name)
+    with open(file=PATH_NEW_USER, mode='a', encoding='utf-8') as new_user_file:
+        time_now = datetime.datetime.now()
+        new_user = PATH_TEMP_NEW_USER.format(RATING=RATING, TIME=time_now)
+        new_user_file.write(new_user)
+    return RATING, time_now
+
+
 def start():
     name = input('name:')
     list_users = os.listdir(path=PATH_PLAYERS)
     if name in list_users:
-        parser_data_user()
+        rating, time = parser_data_user()
     else:
-        PATH_NEW_USER = r"{path}\{user_name}".format(path=PATH_PLAYERS, user_name=name)
-        with open(file=PATH_NEW_USER, mode='a', encoding='utf-8') as new_user_file:
-            time_now = datetime.datetime.now()
-            new_user = PATH_TEMP_NEW_USER.format(RATING=RATING, TIME=time_now)
-            new_user_file.write(new_user)
+        rating, time = new_user(name=name)
+    print_status = PATH_TEMP_START_STATUS.format()
+    print(PATH_TEMP_START_STATUS)
 
 
 def main():
